025c264fdeb461fa7253cbdfa86d8a8c1eb6465b
shriyanshu raj <108788182+rajshriyanhu@users.noreply.github.com>
2023-08-13 17:33:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ mongoose
 
 const io = require("socket.io")(3001, {
   cors: {
-    origin: "https://google-docs-clone-72aqqyrj7-rajshriyanhu.vercel.app",
+    origin: ["https://google-docs-clone-72aqqyrj7-rajshriyanhu.vercel.app",
+             "https://google-docs-clone-git-main-rajshriyanhu.vercel.app"],
     methods: ["GET", "POST"],
     allowedHeaders: ["*"], // You can adjust allowed headers as needed
     credentials: true, // You might need this if you're dealing with cookies or authentication
