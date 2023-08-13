56f63909e0e8a3b82da92874387915c9fb6597f4
shriyanshu raj <108788182+rajshriyanhu@users.noreply.github.com>
2023-08-13 17:15:30 +0530

    Update TextEditor.js

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -25,7 +25,7 @@ const TextEditor = () => {
     // console.log(documentId)
 
     useEffect(() => {
-        const s = io("http://localhost:3001");
+        const s = io("https://docs-clone-29er.onrender.com");
         setSocket(s);
         return () => {
             s.disconnect()
